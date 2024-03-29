24ed92397fa255878c4cfc81ddb1c3f86a39f83c
castecabas <cardenas20220113@gmail.com>
2024-03-28 22:19:47 -0500

    tercer commit - version 1.0 {se añade metdo perfil}

--- src/main/java/com/proyecto/Tienda_ropa_JL/Controllers/ClientController.java
+++ src/main/java/com/proyecto/Tienda_ropa_JL/Controllers/ClientController.java
@@ -31,6 +31,22 @@ public class ClientController {
         return "redirect:/Tienda/Empleados";
     }
 
+    @PostMapping("/Perfil/editar")
+    public String EditarPerfil(@RequestParam("id") String id, @ModelAttribute Client client) {
+        Long ID = Long.parseLong(id);
+        Client oldClient = R_Client.findById(ID).orElse(null);
+
+        if (oldClient != null && client != null) {
+            oldClient.setNombre(client.getNombre());
+            oldClient.setApellido(client.getApellido());
+            oldClient.setTelefono(client.getTelefono());
+            oldClient.setRol(client.getRol());
+            R_Client.save(oldClient);
+            return "redirect:/Tienda/store";
+        }
+        return "redirect:/Tienda/store?error=empleado_no_encontrado"; 
+    }
+
     @PostMapping("/Empleado/editar")
     public String EditarEmpleado(@RequestParam("id") String id, @ModelAttribute Client client) {
     Long ID = Long.parseLong(id);
@@ -46,6 +62,8 @@ public class ClientController {
         R_Client.save(oldClient);
         return "redirect:/Tienda/Empleados"; 
     }
+    
+
     return "redirect:/Tienda/Empleados?error=empleado_no_encontrado"; 
 }
 
